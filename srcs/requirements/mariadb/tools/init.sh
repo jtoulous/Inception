4fdae168d205a8706345ce07ef5fc110e08210f2
jtoulous <toulouse.joshua97@gmail.com>
2023-11-29 23:25:09 +0100

    nginx

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,3 +1,14 @@
-mysql -e "CREATE DATABASE IF NO EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NO EXISTS \`${SQL_USER}\`@`localhost` IDENTIFIED BY \`${SQL_USER_PWD}\`;"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.*"
+#!/bin/bash
+
+service mariadb start
+sleep 10
+
+mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
+mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PWD}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PWD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_PWD}';"
+mysql -u root -p${SQL_PWD} -e "FLUSH PRIVILEGES;"
+
+kill -TERM $(cat /run/mysqld/mysqld.pid)
+
+mysqld
--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -1,4 +1,3 @@
-
 #!/bin/sh
 
 sed -i 's|PHP_PORT|'${PHP_PORT}'|g' /etc/php/7.3/fpm/pool.d/www.conf
@@ -13,8 +12,8 @@ else
   chmod +x wp-cli.phar
   mv wp-cli.phar /usr/local/bin/wp
   wp core download --path=$WP_PATH --allow-root
-  wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --path=$WP_PATH --skip-check --allow-root
-  wp core install --path=$WP_PATH --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL --skip-email --allow-root
+  wp config create --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PWD --dbhost=mariadb --path=$WP_PATH --skip-check --allow-root
+  wp core install --path=$WP_PATH --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$WP_PWD --admin_email=$WP_EMAIL --skip-email --allow-root
   wp theme install teluro --path=$WP_PATH --activate --allow-root
   wp user create jtoulous jtoulou@jo.jo --role=author --path=$WP_PATH --user_pass=jtoulous --allow-root
 
