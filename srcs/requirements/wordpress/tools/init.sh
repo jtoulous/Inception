0efadbb0ab79fd4992ae7028c91e22ac925078af
jtoulous <toulouse.joshua97@gmail.com>
2023-11-30 23:40:09 +0100

    wesh

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -9,13 +9,6 @@ then
 
 else
   wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-  chmod +x wp-cli.phar
-  mv wp-cli.phar /usr/local/bin/wp
-  wp core download --path=$WP_PATH --allow-root
-  wp config create --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PWD --dbhost=mariadb --path=$WP_PATH --skip-check --allow-root
-  wp core install --path=$WP_PATH --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$WP_PWD --admin_email=$WP_EMAIL --skip-email --allow-root
-  wp theme install teluro --path=$WP_PATH --activate --allow-root
-  wp user create jtoulous jtoulou@jo.jo --role=author --path=$WP_PATH --user_pass=jtoulous --allow-root
 
 fi
 
