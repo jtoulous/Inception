a199cb1c833efe85b796797eef5576d2de7e523a
jtoulous <toulouse.joshua97@gmail.com>
2023-12-19 09:11:39 -0500

    end

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 
-
-service mysql start 
-
-
-echo "CREATE DATABASE IF NOT EXISTS $db1_name ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
-echo "FLUSH PRIVILEGES;" >> db1.sql
-
-mysql < db1.sql
+if [ ! -f db1.sql ]; then
+    
+    chmod 644 /etc/mysql/mariadb.conf.d/50-server.cnf 
+    
+    service mariadb start
+    sleep 7
+    
+    echo "CREATE DATABASE IF NOT EXISTS $db_name ;" > db1.sql
+    echo "CREATE USER IF NOT EXISTS '$db_admin'@'%' IDENTIFIED BY '$db_admin_pwd' ;" >> db1.sql 
+    echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_user_pwd' ;" >> db1.sql
+    echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_admin'@'%' ;" >> db1.sql
+    echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;" >> db1.sql
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$root_pwd' ;" >> db1.sql
+    echo "FLUSH PRIVILEGES;" >> db1.sql
+    
+    mysql < db1.sql
+fi
 
 kill $(cat /var/run/mysqld/mysqld.pid)
-
 mysqld
